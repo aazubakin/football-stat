551339a823b99d428bcfcea6b079ffd1713e42ed
Aleksandr Zubakin <eldoson@yandex.ru>
2021-05-08 16:53:10 +0500

    feat: add tailwind library

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,29 @@
 <template>
-  <div>
-    <form @submit.prevent="submit">
-      <p>Enter your API token from football-data.org here</p>
-      <input type="text" v-model="token" placeholder="Your token"/>
-      <input type="submit" value="Submit" />
-      <p v-if="errors.length">
-        <b>Please correct the following error(s):</b>
+  <div class="flex justify-center font-sans">
+    <form class="grid w-px=400" @submit.prevent="submit">
+      <label for="token">
+        Enter your API token from football-data.org here
+      </label>
+      <input
+        class="my-3"
+        name="token"
+        type="text"
+        v-model="token"
+        placeholder="Your token"
+      />
+      <input
+        class="p-2 bg-blue-700 hover:bg-blue-500 c shadow-md cursor-pointer text-blue-50 rounded"
+        type="submit"
+        value="Submit"
+      />
+      <div class="my-3" v-if="errors.length">
+        <p>Please correct the following error(s):</p>
         <ul>
-          <li v-for="(error, idx) in errors" :key="idx" >
+          <li class="text-red-500" v-for="(error, idx) in errors" :key="idx">
             {{ error }}
           </li>
         </ul>
-      </p>
+      </div>
     </form>
   </div>
 </template>
@@ -29,10 +41,11 @@ export default {
   },
   methods: {
     submit() {
+      this.errors = [];
       if (!this.token) {
         this.errors.push("Token required");
-      } else if (this.token.length  < 22) {
-        this.errors.push("Invalid token, length must be more than 21");
+      } else if (this.token.length < 22) {
+        this.errors.push("Invalid token, length must be more than 21 symbols");
       }
       if (this.errors.length === 0) {
         apiClient
